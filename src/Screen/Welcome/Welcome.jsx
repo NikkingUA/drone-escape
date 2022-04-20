b8527e9e4e2a37ccd17d9b0f83e7ac4d1ec49d42
Paolo <pascuccip9@gmail.com>
2022-04-20 15:34:24 +0200

    Add sliderIdea

--- src/Screen/Welcome/Welcome.jsx
+++ src/Screen/Welcome/Welcome.jsx
@@ -1,16 +1,38 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+
+import UiButton from "../../Components/FunctionComponents/UiButton/UiButton";
+import UiInput from "../../Components/FunctionComponents/UiInput/UiInput";
+import UiHeader from "../../Components/FunctionComponents/UiHeader/UiHeader";
+import UiSocial from "../../Components/FunctionComponents/UiSocial/UiSocial";
+
 import './Welcome.scss'
 
 
-const Welcome = (props) => {
-    const navigate = useNavigate();
+function Welcome() {
 
-    return <>
-        <button onClick={() => { navigate("/Tutorial") }} className="bg-red">
-            Vai al Tutorial
-        </button>
-    </>;
+
+    function CbInput(e) {
+        console.log(e);
+
+    }
+
+    return <div className="Welcome flex">
+        {/* <div class="scrolling-image-container">
+            <div class="scrolling-image"></div>
+        </div> */}
+        <UiHeader />
+
+        <UiInput
+            callback={CbInput}
+            placeeholder="Inserisci il tuo nome"
+        />
+
+        <UiButton
+            navigate="/Tutorial"
+            label="Vai al Tutorial"
+        />
+        <UiSocial />
+    </div>;
 };
 
 export default Welcome;
