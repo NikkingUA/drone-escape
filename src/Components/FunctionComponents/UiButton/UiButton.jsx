1bfd56e035771ab2fdc37c1a9f40e0a96163d3b8
Federico Frasca <federico.frasca796@gmail.com>
2022-04-20 11:05:28 +0200

    add UiButton component

--- src/Components/FunctionComponents/UiButton/UiButton.jsx
+++ src/Components/FunctionComponents/UiButton/UiButton.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { PropTypes } from "prop-types";
+
+import './UiButton.scss'
 
 const UiButton = (props) => {
-  return <></>;
+
+    const handleClick = (e) => {
+        props.callback(e);
+    }
+
+    return <>
+        <button
+            type={props.type}
+            onClick={handleClick}
+            className={`nes-btn ${props.class}`}
+            style={props.style}
+        >
+            {props.innerText}
+        </button>
+    </>;
 };
+
+UiButton.defaultProps = {
+    innerText: "Button"
+}
+
+UiButton.propTypes = {
+    callback: PropTypes.func.isRequired,
+    type: PropTypes.string,
+    class: PropTypes.string,
+    style: PropTypes.object,
+
+}
 
 export default UiButton;
--- src/Screen/Game/Game.jsx
+++ src/Screen/Game/Game.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 
+import UiButton from "../../Components/FunctionComponents/UiButton/UiButton";
+
 class Game extends Component {
-  render() {
-    return <></>;
-  }
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    clickTest = (e) => {
+        console.log(e);
+    }
+
+    render() {
+        return <>
+            <UiButton
+                callback={this.clickTest}
+                class={"is-success"}
+                style={{ color: "grey" }}
+                innerText={"Button text"}
+            />
+
+        </>;
+    }
 }
 
 export default Game;
