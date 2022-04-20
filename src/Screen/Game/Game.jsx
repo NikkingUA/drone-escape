d55996ba20df654f232505f7aad4d6497b280947
MykytaPostoienko <nikita.postoienko@gmail.com>
2022-04-20 17:22:58 +0200

    parllax

--- src/Screen/Game/Game.jsx
+++ src/Screen/Game/Game.jsx
@@ -3,32 +3,34 @@ import React, { Component } from "react";
 import UiButton from "../../Components/FunctionComponents/UiButton/UiButton";
 import UiInput from "../../Components/FunctionComponents/UiInput/UiInput";
 
-import "../../Screen/Welcome/Welcome.scss"
+import "./game.scss";
 class Game extends Component {
+  // constructor(props) {
+  //     super(props);
 
-    // constructor(props) {
-    //     super(props);
+  // }
 
-    // }
+  test = (e) => {
+    console.log(e);
+  };
 
-    test = (e) => {
-        console.log(e);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="sky height">
-                    <div className="backhouses height">
-                        <div className="road height">
-                            <div className="crosswalk height">
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div className="container">
+        <div className="sky">
+          <div className="backhouses">
+            <div className="front-houses2">
+              <div className="front-houses1">
+                <div className="road">
+                  <div className="crosswalk"></div>
                 </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
