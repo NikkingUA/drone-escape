00d2b09003eacbfd2b83623cd4daeb1eac56e003
Federico Frasca <federico.frasca796@gmail.com>
2022-04-20 10:31:22 +0200

    install sass

--- src/Screen/Welcome/Welcome.jsx
+++ src/Screen/Welcome/Welcome.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import './Welcome.scss'
+
 
 const Welcome = (props) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return <>
-              {<button onClick={() => { navigate("/Tutorial") }}>
-                Vai al Tutorial
-            </button>
-            }
-  </>;
+    return <>
+        <button onClick={() => { navigate("/Tutorial") }} className="bg-red">
+            Vai al Tutorial
+        </button>
+    </>;
 };
 
 export default Welcome;
