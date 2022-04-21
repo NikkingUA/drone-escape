915ad98216cd21dd6a2b993bdee9f8c13ca15df9
Federico Frasca <federico.frasca796@gmail.com>
2022-04-21 17:57:11 +0200

    re-engineer character fall and jump

--- src/Screen/Game/Game.jsx
+++ src/Screen/Game/Game.jsx
@@ -11,24 +11,60 @@ class Game extends Component {
         this.drone = null;
         this.drone_height = null;
         this.maxOffset = null;
+        this.descending = null;
+        this.ascending = null;
+        this.smooth_jump = null;
+        this.state = {
+            y: 0
+        }
     }
 
     componentDidMount() {
         this.drone = document.getElementById("drone");
         this.drone_height = this.drone.offsetHeight;
 
-        // console.log(drone_height);
         this.maxOffset = window.innerHeight / 2 - this.drone_height / 2;
 
-        this.drone.style.setProperty("--y", this.maxOffset + "px");
+
+        this.descending = setInterval(() => {
+            this.setState({
+                y: this.state.y + 10
+            })
+        }, 30);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(this.maxOffset, this.state.y);
+        // console.log(this.state.y);
+
+        if (this.state.y > this.maxOffset) {
+            console.log("STOP");
+            clearInterval(this.descending)
+        }
+
     }
 
     jump = (e) => {
+        let deltaY = 0;
 
-        this.drone.style.setProperty("--y", -this.maxOffset + "px");
+        if (this.state.y < -this.maxOffset) {
+            console.log("STOP");
+            clearInterval(this.descending)
+        } else {
+            deltaY = 200
+        }
 
-        // setTimeout(() => this.drone.style.setProperty("--y", this.maxOffset + "px"), 1000)
 
+        this.smooth_jump = setInterval(() => {
+            this.setState({
+                y: this.state.y - deltaY
+            })
+        }, 30);
+        clearInterval(this.smooth_jump);
+
+        /* this.setState({
+            y: this.state.y - deltaY
+        }) */
     }
 
     render() {
@@ -49,7 +85,9 @@ class Game extends Component {
                     </div>
                 </div>
 
-                <div id="drone" className="drone scroll_smooth">
+                <div id="drone" className="drone scroll_smooth" style={{
+                    transform: `translate(-50%, -50%) translateY(${this.state.y}px) scale(2)`
+                }}>
                 </div>
             </div>
         );
