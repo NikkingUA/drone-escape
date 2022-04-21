eb4231a6c30528ea9d44990b8ec6135d94a43e58
Federico Frasca <federico.frasca796@gmail.com>
2022-04-21 11:55:40 +0200

    start animating descending drone

--- src/Screen/Game/Game.jsx
+++ src/Screen/Game/Game.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-import "./game.scss";
+import v from "./game.scss";
 class Game extends Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    // }
+    }
+
+    componentDidMount() {
+        // console.log('mount');
+
+        const drone = document.getElementById("drone");
+
+        console.log(window.innerHeight);
+        drone.style.setProperty("--y", window.innerHeight / 2 + "px");
+    }
 
     test = (e) => {
         console.log(e);
@@ -15,20 +24,20 @@ class Game extends Component {
             <div className="container">
 
                 <div className="parallax">
-                <div className="sky bg">
-                    <div className="backhouses bg">
-                        <div className="front-houses2 bg">
-                            <div className="front-houses1 bg">
-                                <div className="road bg">
-                                    <div className="crosswalk bg"></div>
+                    <div className="sky bg">
+                        <div className="backhouses bg">
+                            <div className="front-houses2 bg">
+                                <div className="front-houses1 bg">
+                                    <div className="road bg">
+                                        <div className="crosswalk bg"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                </div>
 
-                <div className="drone scroll_smooth">
+                <div id="drone" className="drone scroll_smooth">
                 </div>
             </div>
         );
